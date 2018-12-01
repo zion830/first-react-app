f2ea5a40098eac879d3b93669abe084e2c7ade19
yunji <zion830@naver.com>
2018-12-01 12:49:16 +0900

    Make to do list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import TodoList from './TodoList';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <TodoList/>
+        );
+    }
+}
+
+export default App;
